19d326cba20392ce35e88a86a3f9beb1e21e6d4e
DanielPons <danielpons44@gmail.com>
2025-07-12 19:39:55 -0400

    Corrigiendo configuración para Vercel deployment

--- sma/wsgi.py
+++ sma/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sma.settings')
 
 application = get_wsgi_application()
+
+# Vercel compatibility
+app = application
